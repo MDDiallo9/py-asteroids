8ebde476cbd40ae05d6874088ae5f1a95c29ddf0
Mohamed LN-WSL <114567817+MDDiallo9@users.noreply.github.com>
2025-01-20 17:52:41 +0100

    Added movement + collisions

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import os
 os.environ["SDL_VIDEODRIVER"]="x11"
 import pygame
 from constants import *
+from circleshape import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+
 
 def main():
     pygame.init()
@@ -9,10 +14,41 @@ def main():
     print(f"""Screen width: {SCREEN_WIDTH}
 Screen height: {SCREEN_HEIGHT}
 """)
+    on = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True :
-        
-        pygame.display.flip()
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids,updatable, drawable)
+    AsteroidField.containers = (updatable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+    
 
+    while on :
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        for entity in updatable:
+            entity.update(dt)
+            
+        for entity in drawable:
+            entity.draw(screen)
+        
+        for asteroid in asteroids:
+            if asteroid.collisionCheck(player) :
+                on = False
+        """ player.update(dt)
+        player.draw(screen) """
+        """ testA.draw(screen)
+        testA.x += 1 """
+        pygame.display.flip()
+        dt =  clock.tick(60) / 1000
+        """ print(f"dt: {dt:.4f} seconds, FPS: {1/dt:.2f} rot: {player.rotation} ast: {asteroids}") """
+        print(f"Drawable group size: {len(drawable)}")
 if __name__ == "__main__":
     main()
